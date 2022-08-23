73a05f277c698dd2b669f3de9633adaf15f47bf8
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-08-23 15:04:22 +1200

    Create basic_var_and_output_with_labels.py

---
+++ Lesson-1/basic_var_and_output_with_labels.py
@@ -0,0 +1,20 @@
+# T M Howe
+# variables
+
+# Declaration of variables
+name = "elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+# Output to screen
+print("one of the big five animals in africa is called",name,'.')
+print("an",name,"has",numLegs,"legs.")
+print("it's height is approximately",height,"metres.")
+print("and, yes, it's",hasTail,". it has a tail.")
+
+
+# Replacing a variable value
+numlegs = 3
+print(numLegs)
+print(numlegs)
