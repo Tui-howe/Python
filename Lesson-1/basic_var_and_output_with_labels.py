6c7324f724b48ffcad138f9839bb4b3a8869522b
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-08-23 15:07:52 +1200

    Update basic_var_and_output_with_labels.py

--- Lesson-1/basic_var_and_output_with_labels.py
+++ Lesson-1/basic_var_and_output_with_labels.py
@@ -4,17 +4,13 @@
 # Declaration of variables
 name = "elephant"
 numLegs = 4
-height = 4.5
+height = 3.8
 hasTail = True
 
 # Output to screen
-print("one of the big five animals in africa is called",name,'.')
-print("an",name,"has",numLegs,"legs.")
-print("it's height is approximately",height,"metres.")
-print("and, yes, it's",hasTail,". it has a tail.")
+print("One of the big five animals in africa is called",name,'.')
+print("An",name,"has",numLegs,"legs.")
+print("It's height is approximately",height,"metres.")
+print("And, yes, it's",hasTail,".", "it has a tail.")
 
 
-# Replacing a variable value
-numlegs = 3
-print(numLegs)
-print(numlegs)
