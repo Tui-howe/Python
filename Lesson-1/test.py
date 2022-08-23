294c47243684e1c6300a4e44d2439e4df477e317
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-08-23 14:31:16 +1200

    Add files via upload

---
+++ Lesson-1/test.py
@@ -0,0 +1,12 @@
+# Declaration of Variables
+
+base = 5.4
+height = 3
+
+# The statement to calculate the area (using variables)
+
+(base * height)/2
+
+# Print the result to screen
+
+print(base * height)/2
