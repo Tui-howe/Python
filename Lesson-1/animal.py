da343296f786facf1e51351bf22c05ea7e00b674
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-08-23 14:26:06 +1200

    Add files via upload

---
+++ Lesson-1/animal.py
@@ -0,0 +1,15 @@
+# variables
+
+name = 'lion'
+numLegs=4
+hasFur='True'
+height=1.2
+
+#prepare print statements
+print(type(name))
+print(type(numLegs))
+print(type(hasFur))
+print(type(height))
+
+
+#construct a readable sentence
