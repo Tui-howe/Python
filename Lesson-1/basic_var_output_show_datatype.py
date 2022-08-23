e46eb4350722cc4a9ce17cc298152714504b7ce6
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-08-23 14:51:17 +1200

    Create basic_var_output_show_datatype.py

---
+++ Lesson-1/basic_var_output_show_datatype.py
@@ -0,0 +1,14 @@
+# T M Howe
+#Variables
+
+# Declaration of variables
+name = "elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+# Output data type to screen
+Print (type(name))
+Print(type(numLegs))
+Print(type(height))
+Print(type(hasTail))
