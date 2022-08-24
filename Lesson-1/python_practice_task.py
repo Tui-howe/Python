285962191cae83a5e954c290a9b6bf2db4c07eaf
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-08-25 10:09:39 +1200

    Create python_practice_task.py

---
+++ Lesson-1/python_practice_task.py
@@ -0,0 +1,26 @@
+# T M Howe
+# Variables, output, labels, datatypes
+
+# Declaration of variables
+from email.headerregistry import Address
+
+
+name = "Tui Marvel Howe"
+height = 186.5
+AddressNumber = 10,2
+AddressStreet = "StoneStreet"
+PhoneNumber = "027 395 2124"
+Allegry = "Dairy"
+
+# Output of variables to screen
+print(name)
+print(height)
+print(AddressNumber)
+print(AddressStreet)
+print(PhoneNumber)
+print(Allegry)
+# Output of variables with labels to screen
+print()
+print()
+print()
+print()
