9b066c0694f153094bd474d42ca947e548de309d
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-09-05 13:28:55 +1200

    Update python_practice_task.py

--- Lesson-1/python_practice_task.py
+++ Lesson-1/python_practice_task.py
@@ -1,10 +1,7 @@
 # T M Howe
-# Variables, output, labels, datatypes
+# Focus - variables, comments, labels, data types.
 
-# Declaration of variables
-from email.headerregistry import Address
-
-
+#Variables
 name = "Tui Marvel Howe"
 height = 186.5
 AddressNumber = 10,2
@@ -12,15 +9,16 @@ AddressStreet = "StoneStreet"
 PhoneNumber = "027 395 2124"
 Allegry = "Dairy"
 
-# Output of variables to screen
+# A summary of this information should be printed out to the screen
+# output = print()
 print(name)
 print(height)
 print(AddressNumber)
 print(AddressStreet)
 print(PhoneNumber)
 print(Allegry)
-# Output of variables with labels to screen
-print()
-print()
-print()
-print()
+
+# Clarify the problem
+# name (var)
+"height is measured in cm"
+""
