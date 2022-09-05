06da85e845605afe1947004b36e2b4edaab6b175
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-09-05 13:40:33 +1200

    Update python_practice_task.py

--- Lesson-1/python_practice_task.py
+++ Lesson-1/python_practice_task.py
@@ -4,7 +4,7 @@
 #Variables
 name = "Tui Marvel Howe"
 height = 186.5
-AddressNumber = 10,2
+AddressNumber = "10/2"
 AddressStreet = "StoneStreet"
 PhoneNumber = "027 395 2124"
 Allegry = "Dairy"
@@ -21,4 +21,21 @@ print(Allegry)
 # Clarify the problem
 # name (var)
 "height is measured in cm"
-""
+"The address is broken up into Street Number and Street, i am assuming that they know im living wellington"
+"I have decided to name what allergy's I have"
+
+# height - expressed in Cenitmetres
+
+
+
+
+
+#Address
+#Street Number, Street
+
+
+#phone number - Mobile
+#stored as a string - no calc
+
+
+#Allergy
