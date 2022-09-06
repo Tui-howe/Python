9005e1b174d87cfba7068dc480b508dc6b82e488
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-09-06 14:52:30 +1200

    Create user_input.py

---
+++ Lesson-2/user_input.py
@@ -0,0 +1,35 @@
+# T M Howe
+# User input types
+
+# Taking input from user
+# String = input()
+# Output
+# Print(string)
+
+# Taking input from the user
+# Name = input("enter your name here")
+# Output
+# Print(name)
+
+# Taking input from the user
+# Name = input("enter your name here: ")
+# Output
+# Print(name)
+
+# Taking input from user as integer
+# Num = int(input("Enter a number: "))
+# Add = num + 1
+# output
+# Print(add)
+
+# Taking input from user as integer
+# Num = Float(input("Enter a number: "))
+# Add = num + 1
+# output
+# Print(add)
+
+# Taking input from user as integer
+# Li = List(input("Enter a number: "))
+# Add = num + 1
+# output
+# Print(Li)
