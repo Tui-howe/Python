453c48593e89d24e41e2cbad1f1602ec8f813e57
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-09-08 09:30:26 +1200

    Create Problem_1.py

---
+++ Lesson-2/Problem_1.py
@@ -0,0 +1,8 @@
+# Problem 01
+# This progam is designed to track and total goals scored by the RC 2nd XI for each game played
+
+game1_pats = 7
+game2_coll = 5
+game3_wellhigh = 10
+teamtotal = game1_pats + game2_coll + game3_wellhigh
+print("Total goals scored: ", teamtotal)
