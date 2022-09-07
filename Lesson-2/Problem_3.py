3c0dea844e9e7931fe1e58dcba872b1d58c495d4
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-09-08 10:09:52 +1200

    Create Problem_3.py

---
+++ Lesson-2/Problem_3.py
@@ -0,0 +1,15 @@
+dieselPrice = 1.47
+# Remember variables cannot begin with a digit
+petrol95Price = 2.17
+petrol91Price = 2.06
+print("intial price:")
+print("Diesel price $" + str (dieselPrice))
+print("95 price $" + str (petrol95Price))
+print("91 unleaded price $" + str (petrol91Price))
+dieselPrice = dieselPrice * 1.1
+petrol95Price = petrol95Price * 1.1
+petrol91Price = petrol91Price * 1.1
+print("Price increase of 10%")
+print("Diesel price $" + str (dieselPrice))
+print("95 price $" + str (petrol95Price))
+print("91 unleaded price $" + str (petrol91Price))
