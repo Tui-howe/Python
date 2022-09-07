2658ec3eaa33c19991a85dabc7d0f0005601dc25
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-09-08 09:57:31 +1200

    Create Problem_2.py

---
+++ Lesson-2/Problem_2.py
@@ -0,0 +1,15 @@
+# T M Howe
+# concatenating string data types in the output
+
+# Problem 2
+# This progam is designed to track the value remaining on a gift card as it is used.
+
+card_balance = 50.00
+Purchase_01 = 35.00
+
+
+print("Your current balance $" + str (card_balance))
+
+Card_Balance = card_balance - Purchase_01
+print("Purchase_01 $" + str (Purchase_01))
+print("Your new balance is $" + str (Card_Balance))
