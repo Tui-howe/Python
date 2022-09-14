22cebaa759ebf31cae055d186dc277600fa0da01
Tui-howe <103474894+Tui-howe@users.noreply.github.com>
2022-09-15 09:15:26 +1200

    Create calcGUI.py

---
+++ Lesson-2/calcGUI.py
@@ -0,0 +1,60 @@
+# T M Howe
+
+# ***************************************************
+# Python Calculator with GUI (Graphic User Interface)
+# ***************************************************
+
+from tkinter import * # import the tkinter library
+
+def button_press(num):
+    pass
+
+def equals():
+    pass
+
+def clear():
+    pass
+
+window = Tk()
+window.title("Calculator Program")
+window.geometry("500x500")
+window.configure(bg="light grey")
+
+equation_text = ""
+
+equation_label = StringVar()
+
+label = Label(window, textvariable=equation_label, font=('console', 20), bg="white", width=24, height=1)
+label.pack()
+
+frame = Frame(window)
+frame.pack()
+
+button1 = Button(frame, text=1, height=4, width=9, font=35, command=lambda: button_press(1))
+button1.grid(row=0, column=0)
+
+button2 = Button(frame, text=2, height=4, width=9, font=35, command=lambda: button_press(2))
+button2.grid(row=0, column=1)
+
+button3 = Button(frame, text=3, height=4, width=9, font=35, command=lambda: button_press(3))
+button3.grid(row=0, column=2)
+
+button4 = Button(frame, text=4, height=4, width=9, font=35, command=lambda: button_press(4))
+button4.grid(row=1, column=0)
+
+button5 = Button(frame, text=5, height=4, width=9, font=35, command=lambda: button_press(5))
+button5.grid(row=1, column=1)
+
+button6 = Button(frame, text=6, height=4, width=9, font=35, command=lambda: button_press(6))
+button6.grid(row=1, column=2)
+
+button7 = Button(frame, text=7, height=4, width=9, font=35, command=lambda: button_press(7))
+button7.grid(row=2, column=0)
+
+button8 = Button(frame, text=8, height=4, width=9, font=35, command=lambda: button_press(8))
+button8.grid(row=2, column=1)
+
+button9 = Button(frame, text=9, height=4, width=9, font=35, command=lambda: button_press(9))
+button9.grid(row=2, column=2)
+
+window.mainloop()
